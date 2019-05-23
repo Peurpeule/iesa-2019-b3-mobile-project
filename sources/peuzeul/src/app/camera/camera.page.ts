8672a1ef41e260f25fccffa1e81c0fc1f8635523
Jeremie <feinglong@gmail.com>
2019-05-23 13:51:54 +0200

    add features getimage()

--- sources/peuzeul/src/app/camera/camera.page.ts
+++ sources/peuzeul/src/app/camera/camera.page.ts
@@ -39,6 +39,23 @@ export class CameraPage implements OnInit {
     }, (err) => {
       // Handle error
     });
+  }
 
+  getPhoto() {
+    const options: CameraOptions = {
+      quality: 70,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then(imageData => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      // this.myphoto = 'data:image/jpeg;base64,' + imageData;
+      this.myphoto = (<any>window).Ionic.WebView.convertFileSrc(imageData);
+    }, (err) => {
+      // Handle error
+    });
   }
 }
