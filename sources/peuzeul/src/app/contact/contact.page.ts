272ff7b9d1a7c45528562551026d47d7ae7bd394
Zentael <charlielucas8@gmail.com>
2019-05-23 15:06:25 +0200

    fx contacts

--- sources/peuzeul/src/app/contact/contact.page.ts
+++ sources/peuzeul/src/app/contact/contact.page.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { GamificationBadgeService } from '../gamification-badge.service'
+import { Component } from '@angular/core';
+import { GamificationBadgeService } from '../gamification-badge.service';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.page.html',
   styleUrls: ['./contact.page.scss'],
+  providers: [Contacts],
 })
-export class ContactPage implements OnInit {
+export class ContactPage {
 
-  constructor(private badge: GamificationBadgeService ) {
-
+  constructor(
+      private badge: GamificationBadgeService,
+      private contacts: Contacts,
+  ) {
+    this.createContact();
   }
 
-  ngOnInit() {
+  contact: Contact = this.contacts.create();
+  createContact() {
+    this.contact.name = new ContactName(null, 'Smith', 'John');
+    this.contact.phoneNumbers = [new ContactField('mobile', '6471234567')];
+    this.contact.save().then(
+        () => console.log('Contact saved!', this.contact),
+        (error: any) => console.error('Error saving contact.', error)
+    );
   }
 
-  ownbadge(){
+  ownbadge() {
     this.badge.reward[1].owned = true;
   }
 
