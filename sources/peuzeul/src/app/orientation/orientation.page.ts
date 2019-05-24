421956a626113a6ab8b732df2afacd051bb0c69c
Jeremie <feinglong@gmail.com>
2019-05-24 09:57:22 +0200

    update Screen plugin

--- sources/peuzeul/src/app/orientation/orientation.page.ts
+++ sources/peuzeul/src/app/orientation/orientation.page.ts
@@ -22,12 +22,10 @@ export class OrientationPage implements OnInit {
 
   setToLandscape(){
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
-    this.orientation = this.screenOrientation.type;
 
   }
 
   setToPortrait(){
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-    this.orientation = this.screenOrientation.type;
   }
 }
